d051e1eccf199dc4857215d1e3dab3673ededa77
amradelata <amradelata@gmail.com>
2020-05-10 04:25:18 +0200

    add popup

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* popup*/
+let mypopup = document.getElementById('mypopup');
+function closepopup(){
+	mypopup.style.display = "none";
+}
+	function shwpopup() {
+	setTimeout(function(){ 
+
+		mypopup.style.display = "block";
+
+	 }, 3000);
+	}
+
+	shwpopup() 
+
+    /* popup*/
+    
+
+    
